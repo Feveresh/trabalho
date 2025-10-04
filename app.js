05aa27de46b715424da7b10843fa2614fd3ddb15
Feveresh <fabricioausilva@gmail.com>
2025-10-03 22:25:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ let foto_troca = false
 function trocaFoto() {
     if (foto_troca === false) {
         for (let i = 0; i < imagem.length; i++) {
-            imagem[i].src = `img/manual${i + 9}.jpeg`
+            imagem[i].src = `manual${i + 9}.jpeg`
         }
         foto_troca = true;
     } else {
         for (let i = 0; i < imagem.length; i++) {
-            imagem[i].src = `img/manual${i + 1}.jpeg`;
+            imagem[i].src = `manual${i + 1}.jpeg`;
         }
         foto_troca = false;
     }
